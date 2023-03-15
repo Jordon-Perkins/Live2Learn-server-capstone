5e731ea21a9e4d29eebc899d4491ee16b68997a9
Jordon Perkins <jordon.rubel@gmail.com>
2023-03-15 13:16:26 -0500

    tag now saves on class

--- live2learnapi/models/Tag.py
+++ live2learnapi/models/Tag.py
@@ -3,4 +3,4 @@ from .ThisClass import ThisClass
 
 class Tag (models.Model):
     tag = models.CharField(max_length=55)
-    this_class = models.OneToOneField(ThisClass, on_delete=models.CASCADE)
+    this_class = models.ForeignKey(ThisClass, on_delete=models.CASCADE)
